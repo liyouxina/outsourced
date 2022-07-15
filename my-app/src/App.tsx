580509138091fbcd7ee8d37b180d1c78b4dc01b7
liyouxin.lyx <liyouxin.lyx@alibaba-inc.com>
2022-07-15 23:29:43 +0800

    try

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,13 +1,56 @@
-import { useState } from 'react'
+import React from 'react';
 import './App.css'
 import Cascader from './component/cascader'
 import MCascader from './component/mcascader'
+interface Option {
+  name: string;
+  value: string;
+  children: Option[];
+  choosen: boolean;
+}
 function App() {
-
+  let options:Option[] = [{
+    name: "zhejiangasdasdasd",
+    value: "zhejiang",
+    children: [{
+      name: "shaoxing",
+      value: "shaoxing",
+      children: [{
+        name: "zhejiang",
+        value: "zhejiang",
+        children: [],
+        choosen: false
+      }],
+      choosen: false
+    },{
+      name: "hangzhou",
+      value: "hangzhou",
+      children: [{
+        name: "yuhang",
+        value: "yuhang",
+        children: [],
+        choosen: false
+      }],
+      choosen: false
+    }],
+    choosen: true
+  },
+  {
+    name: "jiangsu",
+    value: "jiangsu",
+    children: [],
+    choosen: false
+  },
+  {
+    name: "beijing",
+    value: "beijing",
+    children: [],
+    choosen: false
+  }];
   return (
     <div className="App">
       {/* // 仿antd 级联 */}
-      <Cascader></Cascader>
+      <Cascader options={options}></Cascader>
       {/* // 仿antd mobile级联 */}
       <MCascader></MCascader>
     </div>
--- my-app/src/component/cascader/index.tsx
+++ my-app/src/component/cascader/index.tsx
@@ -1,5 +1,56 @@
-export default function Cascader() {
+import "./index.css";
+import { useState } from 'react';
+interface Option {
+  name: string;
+  value: string;
+  children: Option[];
+  choosen: boolean;
+}
+
+export default function Cascader(props: any) {
+  const [show, setShow] = useState(false);
+  const [options, setOptions] = useState<Option[]>([]);
+  //setOptions(props.options);
+  const showCascader = () => {
+    if (!show) {
+      return [];
+    }
+    let result: Option[][] = [];
+
+    let layer = props.options;
+    do {
+      result.push(layer);
+      let nextLayer: Option[] = [];
+      for (let option of layer) {
+        if (option.choosen) {
+          nextLayer.push(...option.children);
+        }
+      }
+      layer = nextLayer;
+    } while (layer.length != 0)
+    console.log(result);
+    return result;
+  }
+  let doms = showCascader();
+
   return (
-    <div>仿antd 级联(PC端级联) </div>
+    <div>
+      <div className="bb" onClick={() => { setShow(true) }}></div>
+      {show && doms.map(options => (
+        <div key={options.toString()} className="innerContainer">
+          {options.map(option => (
+            <div key={option.name} className="option" onClick={(e) => {
+              console.log(option.name + "choose");
+              option.choosen = true;
+              console.log(option.choosen)
+            }}>
+              {option.name}
+            </div>
+          )
+          )}
+        </div>
+      )
+      )}
+    </div>
   )
 }
